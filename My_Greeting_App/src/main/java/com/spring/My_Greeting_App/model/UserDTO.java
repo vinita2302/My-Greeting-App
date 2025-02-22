8518f6fc16e6cfdfe152ccd00fbbcac057e37a12
vinita patanker <vinitapatanker@gmail.com>
2025-02-22 15:51:26 +0530

    [vinita patanker] UC3_Greeting_Message

---
+++ My_Greeting_App/src/main/java/com/spring/My_Greeting_App/model/UserDTO.java
@@ -0,0 +1,30 @@
+package com.spring.My_Greeting_App.model;
+
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+    public UserDTO() {}
+
+    public UserDTO(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

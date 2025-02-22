875fe2aafb887c79e53671e98fa8d05e3b2dd9f1
vinita patanker <vinitapatanker@gmail.com>
2025-02-22 12:28:13 +0530

    My Greetin App

---
+++ My_Greeting_App/src/main/java/com/spring/My_Greetin_App/MyGreetinAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.My_Greetin_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGreetinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGreetinAppApplication.class, args);
+	}
+
+}
---
+++ My_Greeting_App/src/test/java/com/spring/My_Greetin_App/MyGreetinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.My_Greetin_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGreetinAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

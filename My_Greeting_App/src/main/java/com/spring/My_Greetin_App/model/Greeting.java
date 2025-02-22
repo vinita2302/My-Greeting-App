05915a52c0660973a5ca2a024fa508d5ec22abc9
vinita patanker <vinitapatanker@gmail.com>
2025-02-22 15:09:46 +0530

    [vinita patanker]  UC1_GreetingController

---
+++ My_Greeting_App/src/main/java/com/spring/My_Greetin_App/model/Greeting.java
@@ -0,0 +1,17 @@
+package com.spring.My_Greetin_App.model;
+
+public class Greeting {
+    private String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}

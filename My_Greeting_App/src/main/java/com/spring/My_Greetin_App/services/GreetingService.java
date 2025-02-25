0ef365c8cbadc02e953eb5c3f8edbed9cc7ac030
vinita patanker <vinitapatanker@gmail.com>
2025-02-25 10:13:53 +0530

    [vinita patanker] Add :  UC6_Greetin_Message_List

---
+++ My_Greeting_App/src/main/java/com/spring/My_Greetin_App/services/GreetingService.java
@@ -0,0 +1,34 @@
+package com.spring.My_Greetin_App.services;
+
+import com.spring.My_Greetin_App.model.GreetingEntity;
+import com.spring.My_Greetin_App.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GreetingService {
+
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    public String getGreetingMessage() {
+        return "Hello World!";
+    }
+
+    public List<GreetingEntity> getAllGreetings() {
+        return greetingRepository.findAll();
+    }
+
+    public GreetingEntity saveGreetingMessage(String message) {
+        GreetingEntity greeting = new GreetingEntity(message);
+        return greetingRepository.save(greeting);
+    }
+
+    public GreetingEntity getGreetingById(Long id) {
+        Optional<GreetingEntity> greeting = greetingRepository.findById(id);
+        return greeting.orElseThrow(() -> new RuntimeException("Greeting not found with ID: " + id));
+    }
+}

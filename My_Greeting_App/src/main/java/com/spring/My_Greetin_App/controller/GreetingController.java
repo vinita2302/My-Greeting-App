215eb78e9bac72a14443c720aa4a8096cac25aa4
vinita patanker <vinitapatanker@gmail.com>
2025-02-25 10:23:14 +0530

    [vinita patanker] Add :  UC7_Edit_Greeting_Messages

---
+++ My_Greeting_App/src/main/java/com/spring/My_Greetin_App/controller/GreetingController.java
@@ -0,0 +1,60 @@
+package com.spring.My_Greetin_App.controller;
+
+import com.spring.My_Greetin_App.model.Greeting;
+import com.spring.My_Greetin_App.model.GreetingEntity;
+import com.spring.My_Greetin_App.model.UserDTO;
+import com.spring.My_Greetin_App.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hello")
+public class GreetingController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/greeting")
+    public ResponseEntity<Greeting> getGreeting() {
+        return ResponseEntity.ok(new Greeting(greetingService.getGreetingMessage()));
+    }
+
+    @GetMapping("/greetings")
+    public List<GreetingEntity> getAllGreetings() {
+        return greetingService.getAllGreetings();
+    }
+
+    @GetMapping("/greeting/{id}")
+    public ResponseEntity<GreetingEntity> getGreetingById(@PathVariable Long id) {
+        GreetingEntity greeting = greetingService.getGreetingById(id);
+        return ResponseEntity.ok(greeting);
+    }
+
+    @PostMapping("/greeting")
+    public ResponseEntity<GreetingEntity> createGreeting(@RequestBody UserDTO user) {
+        String message = generateGreetingMessage(user.getFirstName(), user.getLastName());
+        GreetingEntity savedGreeting = greetingService.saveGreetingMessage(message);
+        return ResponseEntity.status(201).body(savedGreeting);
+    }
+
+    @PutMapping("/greeting/{id}")
+    public ResponseEntity<GreetingEntity> updateGreeting(@PathVariable Long id, @RequestBody Greeting updatedGreeting) {
+        GreetingEntity updatedEntity = greetingService.updateGreeting(id, updatedGreeting.getMessage());
+        return ResponseEntity.ok(updatedEntity);
+    }
+
+    private String generateGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && !firstName.isEmpty() && lastName != null && !lastName.isEmpty()) {
+            return "Hello " + firstName + " " + lastName + " from BridgeLabz!";
+        } else if (firstName != null && !firstName.isEmpty()) {
+            return "Hello " + firstName + " from BridgeLabz!";
+        } else if (lastName != null && !lastName.isEmpty()) {
+            return "Hello " + lastName + " from BridgeLabz!";
+        } else {
+            return "Hello World!";
+        }
+    }
+}
---
+++ My_Greeting_App/src/main/java/com/spring/My_Greetin_App/model/Greeting.java
@@ -0,0 +1,21 @@
+package com.spring.My_Greetin_App.model;
+
+
+public class Greeting {
+    private String message;
+
+    public Greeting() {
+    }
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
